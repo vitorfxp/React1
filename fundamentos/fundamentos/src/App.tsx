2458be5708a6d7f7d6a921e3f5d3f17bfa097207
João Vitor Ferreira Ribeiro <joaovitorpvh777@gmail.com>
2025-09-03 12:59:08 -0400

    -- fundamentos

---
+++ fundamentos/fundamentos/src/App.tsx
@@ -0,0 +1,14 @@
+import FirstName from './components/FirstComponents'
+import TemplateExpressions from  './components/TemplateExpressions'
+import './App.css'
+
+function App() {
+  return (<div>
+    <h1>Fundamentos</h1>
+    <FirstName />
+    <TemplateExpressions/>
+    </div>
+  )
+}
+
+export default App
---
+++ fundamentos/fundamentos/src/components/FirstComponents.tsx
@@ -0,0 +1,11 @@
+const FirstName = () => {
+    return(
+        <div>
+        <p>ExportandoComponente</p>
+        <p className="teste">amburgui 3</p>
+        {/* Comentario em JSX */}
+        </div>
+        
+    ) }
+
+export default FirstName

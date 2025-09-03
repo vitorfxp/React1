01a15b218f9e970f5637847116807dcf949127f0
João Vitor Ferreira Ribeiro <joaovitorpvh777@gmail.com>
2025-09-03 13:27:59 -0400

    -- commit

--- fundamentos/fundamentos/src/App.tsx
+++ fundamentos/fundamentos/src/App.tsx
@@ -1,12 +1,14 @@
 import FirstName from './components/FirstComponents'
 import TemplateExpressions from  './components/TemplateExpressions'
 import './App.css'
+import Event from './components/Event'
 
 function App() {
   return (<div>
     <h1>Fundamentos</h1>
     <FirstName />
     <TemplateExpressions/>
+    <Event/>
     </div>
   )
 }
--- fundamentos/fundamentos/src/components/FirstComponents.tsx
+++ fundamentos/fundamentos/src/components/FirstComponents.tsx
@@ -1,8 +1,11 @@
+import MyComponent from "./MyComponent"
+
 const FirstName = () => {
     return(
         <div>
         <p>ExportandoComponente</p>
         <p className="teste">amburgui 3</p>
+        <MyComponent/>
         {/* Comentario em JSX */}
         </div>
         
